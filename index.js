e663dac8bcc558afc46272bf1dc0d1654a1d36ed
steve nagle <nagle@ik.me>
2026-05-25 17:46:14 +0200

    Add PayBridge class for payment processing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = require("@payflow/sdk");
+export class PayBridge {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = "https://api.paybridge.buzz";
+  }
+
+  async createPayment(data) {
+    const response = await fetch(`${this.baseUrl}/payments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.apiKey}`
+      },
+      body: JSON.stringify(data)
+    });
+
+    return response.json();
+  }
+}
+
+export default PayBridge;
